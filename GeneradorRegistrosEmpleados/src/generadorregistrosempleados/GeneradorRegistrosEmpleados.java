91c709362c6d969f92e94cb1d5e30d2e0494c22a
s18015565 <57199591+s18015565@users.noreply.github.com>
2019-11-27 00:51:05 -0600

    Colocado campo nss

--- GeneradorRegistrosEmpleados/src/generadorregistrosempleados/GeneradorRegistrosEmpleados.java
+++ GeneradorRegistrosEmpleados/src/generadorregistrosempleados/GeneradorRegistrosEmpleados.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class GeneradorRegistrosEmpleados {
 
+    
    static String[] nombres={"ANTONIO","JOSE","MANUEL","FRANCISCO","DAVID","JUAN","JOSE ANTONIO","JAVIER","DANIEL","JOSE LUIS","FRANCISCO JAVIER","CARLOS","JESUS","ALEJANDRO","MIGUEL","JOSE MANUEL","RAFAEL","MIGUEL ANGEL","PEDRO","ANGEL","PABLO","JOSE MARIA","SERGIO","FERNANDO","LUIS","JORGE","ALBERTO","JUAN CARLOS","ALVARO","JUAN JOSE","ADRIAN","DIEGO","RAUL","JUAN ANTONIO","IVAN","ENRIQUE","RUBEN","RAMON","OSCAR","VICENTE","ANDRES","JUAN MANUEL","JOAQUIN","SANTIAGO","EDUARDO","VICTOR","MARIO","ROBERTO","JAIME","FRANCISCO JOSE","MARCOS","IGNACIO","ALFONSO","JORDI","RICARDO","SALVADOR","HUGO","EMILIO","GUILLERMO","GABRIEL","MARC","JULIO","JULIAN","GONZALO","JOSE MIGUEL","TOMAS","MOHAMED","AGUSTIN","MARTIN","JOSE RAMON","NICOLAS","FELIX","JOAN","ISMAEL","CRISTIAN","SAMUEL","AITOR","HECTOR","JUAN FRANCISCO","JOSEP","MARIANO","LUCAS","JOSE CARLOS","IKER","DOMINGO","SEBASTIAN","ALFREDO","ALEX","CESAR","FELIPE","JOSE ANGEL","JOSE IGNACIO","VICTOR MANUEL","RODRIGO","LUIS MIGUEL","JOSE FRANCISCO","JUAN LUIS","GREGORIO","XAVIER","ALBERT"};
    
    static String[] nombresM={"MARIA CARMEN","MARIA","CARMEN","JOSEFA","ANA MARIA","ISABEL","MARIA PILAR","MARIA DOLORES","LAURA","MARIA TERESA","ANA","CRISTINA","MARIA ANGELES","MARTA","FRANCISCA","ANTONIA","MARIA ISABEL","MARIA JOSE","LUCIA","DOLORES","SARA","PAULA","ELENA","MARIA LUISA","PILAR","RAQUEL","ROSA MARIA","CONCEPCION","MANUELA","MERCEDES","MARIA JESUS","BEATRIZ","JULIA","ROSARIO","NURIA","JUANA","SILVIA","TERESA","ENCARNACION","IRENE","ALBA","PATRICIA","MONTSERRAT","ANDREA","ROCIO","ROSA","MONICA","ALICIA","MARIA MAR","ANGELA","SONIA","SANDRA","MARINA","SUSANA","NATALIA","YOLANDA","MARGARITA","MARIA JOSEFA","CLAUDIA","EVA","MARIA ROSARIO","INMACULADA","MARIA MERCEDES","ANA ISABEL","ESTHER","CARLA","SOFIA","NOELIA","VERONICA","ANGELES","NEREA","CAROLINA","MARIA VICTORIA","EVA MARIA","MARIA ROSA","MIRIAM","INES","LORENA","AMPARO","ANA BELEN","MARIA CONCEPCION","MARIA ELENA","VICTORIA","DANIELA","MARIA ANTONIA","CATALINA","LIDIA","CONSUELO","MARIA NIEVES","CELIA","ALEJANDRA","MARTINA","OLGA","GLORIA","EMILIA","AINHOA","LUISA","FATIMA","MARIA SOLEDAD","AURORA"};
@@ -56,9 +57,9 @@ public class GeneradorRegistrosEmpleados {
    
     public static void main(String[] args) {
         
-        int numeroFilas=50;
+        int numeroFilas=15;
         int numeroCampos=8;
-        String nombreTabla="Cliente";
+        String nombreTabla="Empleado";
         
         System.out.println("INSERT INTO "+nombreTabla+" VALUES");
         
@@ -152,8 +153,8 @@ public class GeneradorRegistrosEmpleados {
                     imprimirColumna(generarTelefono());
                     break;
                     
-                case 8://GENERAR CORREO
-                    imprimirColumnaU(generarCorreo(apellidoP));
+                case 8://GENERAR NSS
+                    imprimirColumnaU(generarCodigo(12));
                     break;
                     
             }
